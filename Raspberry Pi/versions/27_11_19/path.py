96de2b7ee14b074ac9611366d2da82f872cef222
Sailsman <danielgluch33@gmail.com>
2019-11-27 22:10:15 +0100

    start on pathing

--- Raspberry Pi/versions/27_11_19/path.py
+++ Raspberry Pi/versions/27_11_19/path.py
@@ -1,1 +1,12 @@
-def next_path()
+#maze 2020 path module
+import waypoint
+
+def next_path(current_tile, task_to_do):
+    list_of_paths = list
+
+    # todo try ideal path
+
+    network = waypoint.create(current_tile)
+
+    # todo find best
+    # todo return best path
---
+++ Raspberry Pi/versions/27_11_19/waypoint.py
@@ -0,0 +1,7 @@
+#maze 2020 waypoint module
+import tile
+
+
+def create(current_tile):
+    # todo current tile gets first waypoint
+    pass
